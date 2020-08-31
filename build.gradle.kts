0b04eb96a82a1f2de402a9ce25988eec39c124a9
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2020-08-31 14:52:49 +0200

    Better test suite for rgb bias

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
 }
 
 application {
--- src/test/java/tech/guiyom/anscapes/renderer/RgbImageRendererTest.java
+++ src/test/java/tech/guiyom/anscapes/renderer/RgbImageRendererTest.java
@@ -1,8 +1,8 @@
 package tech.guiyom.anscapes.renderer;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import tech.guiyom.anscapes.ColorMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -17,21 +17,23 @@ public class RgbImageRendererTest {
         new File("temp").mkdir();
     }
 
-    @Test
-    public void testRgbColors() throws IOException {
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 2, 4, 8, 16, 32 })
+    public void testRgbColors(final int bias) throws IOException {
 
-        ImageRenderer converter = ImageRenderer.createRenderer(ColorMode.RGB, 64, 64);
+        ImageRenderer converter = new RgbImageRenderer(64, 64, bias);
 
-        FileOutputStream out = new FileOutputStream("temp/shield_rgb.txt");
+        FileOutputStream out = new FileOutputStream("temp/shield_rgb_" + bias + ".txt");
         String result = converter.renderString(ImageIO.read(getClass().getResourceAsStream("shield.png")));
         out.write(result.getBytes(StandardCharsets.UTF_8));
         out.close();
     }
 
-    @Test
-    public void testRgbVideo() throws IOException {
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 2, 4, 8, 16, 32 })
+    public void testRgbVideo(final int bias) throws IOException {
 
-        ImageRenderer converter = ImageRenderer.createRenderer(ColorMode.RGB, 384, 216);
+        ImageRenderer converter = new RgbImageRenderer(360, 360, bias);
 
         BufferedImage img = ImageIO.read(getClass().getResourceAsStream("shield.png"));
         int[] data = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
@@ -40,16 +42,16 @@ public class RgbImageRendererTest {
         for (int i = 0; i < 200; ++i)
             converter.render(data, img.getWidth(), img.getHeight(), (buf, len) -> buf[0] = 'a');
 
-        int numRun = 400;
+        // Bench
+        int numRun = 600;
         long startTime = System.nanoTime();
-
         for (int i = 0; i < numRun; ++i)
             converter.render(data, img.getWidth(), img.getHeight(), (buf, len) -> buf[0] = 'a');
+        long totalTime = System.nanoTime() - startTime;
 
-        long totalTime = (System.nanoTime() - startTime);
-        System.out.printf("RGB Conversion time :%n - Total : %d ms%n - Per frame : %f ms%n - Per pixel : %f ns%n",
+        System.out.printf("RGB (bias=%d) | Total (ms) : %d | Per frame (ms) : %f%n",
+                bias,
                 totalTime / 1_000_000,
-                (double) totalTime / (numRun * 1_000_000),
-                (double) totalTime / (numRun * img.getHeight() * img.getWidth()));
+                (double) totalTime / (numRun * 1_000_000));
     }
 }
